6af27868d1d329f34f736e4bbaf4877e0ea5247b
gargipatel001 <gargipatel001@gmail.com>
2020-07-07 13:25:24 -0400

    Contact and About Us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,53 @@
 import React from "react";
+import Accordion from 'react-bootstrap/Accordion'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 
 const About = () => {
     return (
         <div>
-            <h1> About us</h1>
+            <h3 style={{ textAlign: 'center' }}> About Us</h3>
+            <Row style={{marginTop:'30px',textAlign:'justify'}}>
+                <Col xs={6}>
+                    <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+                </Col>
+                <Col xs={6}>
+                    <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+                    </p>
+                </Col>
+            </Row>
+            <Row style={{marginTop:'30px',marginBottom:'30px'}}>
+                <Col xs={8}>
+                    <Accordion defaultActiveKey="0">
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                    Question 1
+                            </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                        <Card>
+                            <Card.Header>
+                                <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                                    Question 2
+                            </Accordion.Toggle>
+                            </Card.Header>
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
+                </Col>
+            </Row>
         </div>
     );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,61 @@
 import React from "react";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
 
 
 const Contact = () => (
     <div>
-        <h1> Contact us</h1>
+        {/* <Row className="justify-content-md-center">
+        <Col xs lg="2"> */}
+        <Row>
+            <Col xs={8}>
+                <Form>
+                    <Col xs={7}>
+                        <Form.Group controlId="formBasicName">
+                            <Form.Label></Form.Label>
+                            <Form.Control type="name" placeholder="Name" />
+
+                        </Form.Group>
+                        <Form.Group controlId="formBasicEmail">
+
+                            <Form.Control type="email" placeholder="Email" />
+
+                        </Form.Group>
+                        <Form.Group controlId="formBasicMessage">
+
+                            <Form.Control type="subject" placeholder="Subject" />
+
+                        </Form.Group>
+
+                        <Form.Group controlId="formBasicPassword">
+
+                            <Form.Control as="textarea" placeholder="Message.." />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Submit
+  </Button>
+                    </Col>
+                </Form>
+            </Col>
+            <Col>
+                <h3 style={{padding: '10px'}}>Our contact</h3>
+                <Card style={{ width: '18rem' }}>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>Gargi Patel</ListGroup.Item>
+                        <ListGroup.Item>Henil Parikh</ListGroup.Item>
+                        <ListGroup.Item>Harsimran</ListGroup.Item>
+                        <ListGroup.Item>Karan Patel</ListGroup.Item>
+                    </ListGroup>
+                </Card>
+            </Col>
+        </Row>
+
+
     </div>
 )
 
